b10ea21cac945539914a5dfa8711c4191b5ecc79
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-03-11 07:36:53 -0400

    Initialized my variables and set up my random number

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,15 @@
 package com.LickingHeights;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	
+	long secretNum = Math.round(100 * Math.random());
+	int userNum;
+	int userGuess;
+
+	Scanner keyboard = new Scanner(System.in);
+	System.out.println("Guess a number between 1 - 100. ");
+
+	    
     }
 }
