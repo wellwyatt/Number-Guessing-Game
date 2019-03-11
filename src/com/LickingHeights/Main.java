90ef86d497883ba7394bd24a20aa324c1718b9d2
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-03-11 07:58:59 -0400

    Gave my ints values and create my while and if  statements.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,12 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	long secretNum = Math.round(100 * Math.random());
-	int userNum;
-	int userGuess;
+	int userNum = 0;
+	int userGuess = 0;
 
 	Scanner keyboard = new Scanner(System.in);
 	System.out.println("Guess a number between 1 - 100. ");
 
-	    
+	    while (userNum != secretNum){
+	        userGuess++;
+	        userNum = keyboard.nextInt();
+
+	        if (userNum < secretNum ){
+
+	            System.out.println("Number is too low, Try Again.");
+            }
+        }
     }
 }
