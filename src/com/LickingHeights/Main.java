e3a36fc84fb9d3dc75f83ef82fd0b75539277fba
wyatt <wyatt.schlicher.2021@student.lhschools.org>
2019-03-11 08:08:10 -0400

    Finished my Number Guessing Game.

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,30 +1,30 @@
 package com.LickingHeights;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	long secretNum = Math.round(100 * Math.random());
-	int userNum = 0;
-	int userGuess = 0;
+        long secretNum = Math.round(100 * Math.random());
+        int userNum = 0;
+        int userGuesses = 0;
 
-	Scanner keyboard = new Scanner(System.in);
-	System.out.println("Guess a number between 1 - 100. ");
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Guess a number between 1 - 100. ");
 
-	    while (userNum != secretNum){
-	        userGuess++;
-	        userNum = keyboard.nextInt();
+        while (userNum != secretNum) {
+            userGuesses++;
+            userNum = keyboard.nextInt();
 
-	        if (userNum < secretNum ){
-
-	            System.out.println("Number is too low, Try Again!");
+            if (userNum < secretNum) {
+                System.out.println("Too low, Try Again!");
+            } else if (userNum > secretNum) {
+                System.out.println("Too high, Try Again!");
             }
-            else if  (userNum > secretNum){
 
-	            System.out.println("Number is too high, Tyr Again!");
-            }
-	        
 
-	    }
 
+        }
+        System.out.println("You got it! It Took " + userGuesses + " guesses to find it.");
     }
 }
